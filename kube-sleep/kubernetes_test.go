bb14650a978607800b86076c06a708083de6c4b8
Johannes Lohmer <lohmer@univention.de>
2025-06-12 14:09:30 +0200

    test: First kubernetes integration tests

---
+++ kube-sleep/kubernetes_test.go
@@ -0,0 +1,119 @@
+package kubesleep
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+)
+
+func testCluster() (*k8simpl, func() error, error) {
+	k8s := &k8simpl{}
+
+	slog.Debug("Starting a testing kubernetes control plane")
+	testEnv := &envtest.Environment{}
+	cfg, err := testEnv.Start()
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to start test cluster %w", err)
+	}
+
+	k8s.clientset, err = kubernetes.NewForConfig(cfg)
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to create client config for the test cluster %w", err)
+	}
+
+	k8s.ctx, k8s.cancel = context.WithCancel(context.TODO())
+
+	stop := func() error {
+		k8s.cancel()
+		return testEnv.Stop()
+	}
+
+	return k8s, stop, nil
+}
+
+func testNamespace(name string, k8s *k8simpl) (func() error, error) {
+	namespace := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+	}
+	namespace, err := k8s.clientset.CoreV1().Namespaces().Create(k8s.ctx, namespace, metav1.CreateOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create testing namespace: %s %w", name, err)
+	}
+
+	delete := func() error {
+		return k8s.clientset.CoreV1().Namespaces().Delete(k8s.ctx, name, metav1.DeleteOptions{})
+	}
+	return delete, nil
+}
+
+type Integrationtest struct {
+	LoggingSuite
+	stopCluster func() error
+	k8s         *k8simpl
+}
+
+func (s *Integrationtest) SetupSuite() {
+	var err error
+	s.k8s, s.stopCluster, err = testCluster()
+	s.Require().NoError(err)
+}
+
+func (s *Integrationtest) TearDownSuite() {
+	s.stopCluster()
+}
+
+func TestIntegration(t *testing.T) {
+	suite.Run(t, new(Integrationtest))
+}
+
+func (s *Integrationtest) TestCreateDeleteStatefile() {
+	deleteNamespace, err := testNamespace("create-delete-statefile", s.k8s)
+	s.Require().NoError(err)
+	defer deleteNamespace()
+
+	_, err = s.k8s.createStateFile("create-delete-statefile", &suspendStateFile{})
+	s.Require().NoError(err)
+
+	err = s.k8s.deleteStateFile("create-delete-statefile")
+	s.Require().NoError(err)
+}
+
+func (s *Integrationtest) TestUpdateStatefile() {
+	deleteNamespace, err := testNamespace("update-statefile", s.k8s)
+	s.Require().NoError(err)
+	defer deleteNamespace()
+
+	sus := []suspendable{
+		suspendable{
+			manifestType: "Deployment",
+			name:         "testDeployment",
+			replicas:     int32(2),
+		},
+	}
+
+	_, err = s.k8s.createStateFile("update-statefile", &suspendStateFile{
+		suspendables: []suspendable{},
+		finished:     false,
+	})
+	s.Require().NoError(err)
+	defer s.k8s.deleteStateFile("update-statefile")
+
+	_, err = s.k8s.updateStateFile("update-statefile", &suspendStateFile{
+		suspendables: sus,
+		finished:     true,
+	})
+
+	stateFile, err := s.k8s.getStateFile("update-statefile")
+	slog.Debug("Read updated state file from cluster", "stateFile", stateFile)
+	s.Require().True(stateFile.finished)
+	s.Require().Equal(sus, stateFile.suspendables)
+}
---
+++ kube-sleep/testify_test.go
@@ -0,0 +1,34 @@
+package kubesleep
+
+import (
+	"bytes"
+	"log/slog"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type LoggingSuite struct {
+	suite.Suite
+	logBuf    bytes.Buffer
+	oldLogger *slog.Logger
+}
+
+func (ls *LoggingSuite) SetupTest() {
+	ls.logBuf.Reset()
+
+	handler := slog.NewTextHandler(&ls.logBuf, &slog.HandlerOptions{
+		AddSource: false,
+		Level:     slog.LevelDebug,
+	})
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
+}
+
+func (loggingSuite *LoggingSuite) TearDownTest() {
+	if !loggingSuite.T().Failed() || !testing.Verbose() {
+		return
+	}
+	loggingSuite.T().Log("=== Captured Production Logs ===\n")
+	loggingSuite.T().Log(loggingSuite.logBuf.String())
+}
