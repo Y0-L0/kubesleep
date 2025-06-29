77924eaf256ac2bc76c15b21baf43584d5161877
Johannes Lohmer <lohmer@univention.de>
2025-06-29 17:12:38 +0200

    test: Statefile merge tests

--- kubesleep/suspend-state_test.go
+++ kubesleep/suspend-state_test.go
@@ -66,3 +66,42 @@ func (s *Unittest) TestDeserializeStatefileIncompleteJson() {
 		_ = NewSuspendStateFileFromJson(`{"finished": false}`)
 	})
 }
+
+func (s *Unittest) TestMergeStateFiles() {
+	a := Suspendable{1, "a", 1, nil}
+	b := Suspendable{2, "b", 2, nil}
+	c := Suspendable{1, "c", 3, nil}
+	c2 := Suspendable{1, "c", 30, nil}
+	d := Suspendable{2, "d", 4, nil}
+	e := Suspendable{1, "e", 5, nil}
+	existing := SuspendStateFile{
+		map[string]Suspendable{
+			a.Identifier(): a,
+			b.Identifier(): b,
+			c.Identifier(): c,
+		},
+		true,
+	}
+	new := SuspendStateFile{
+		map[string]Suspendable{
+			b.Identifier():  b,
+			c2.Identifier(): c2,
+			d.Identifier():  d,
+			e.Identifier():  e,
+		},
+		false,
+	}
+	expected := &SuspendStateFile{
+		map[string]Suspendable{
+			b.Identifier(): b,
+			c.Identifier(): c,
+			d.Identifier(): d,
+			e.Identifier(): e,
+		},
+		false,
+	}
+
+	actual := existing.merge(&new)
+
+	s.Require().Equal(expected, actual)
+}
