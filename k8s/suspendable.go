9968739a2ae36b482d0ac1f3cb6e7dc43ee1eda9
Johannes Lohmer <lohmer@univention.de>
2025-10-20 00:33:24 +0200

    feat: Parallelize fetching the different types of suspendables (CronJob, Deployment StatefulSet)

--- k8s/suspendable.go
+++ k8s/suspendable.go
@@ -6,6 +6,7 @@ import (
 	"log/slog"
 
 	kubesleep "github.com/Y0-L0/kubesleep/kubesleep"
+	"golang.org/x/sync/errgroup"
 )
 
 func mergeNoOverwrite[K comparable, V any](maps ...map[K]V) map[K]V {
@@ -13,7 +14,7 @@ func mergeNoOverwrite[K comparable, V any](maps ...map[K]V) map[K]V {
 	for _, m := range maps {
 		for k, v := range m {
 			if _, exists := result[k]; exists {
-				panic(fmt.Sprintf("duplicate suspendable identifier: %v", k))
+				panic(fmt.Sprintf("Suspendable Identifier duplicate suspendable identifier: %v", k))
 			}
 			result[k] = v
 		}
@@ -22,16 +23,26 @@ func mergeNoOverwrite[K comparable, V any](maps ...map[K]V) map[K]V {
 }
 
 func (k8s K8Simpl) GetSuspendables(ctx context.Context, namespace string) (map[string]kubesleep.Suspendable, error) {
-	deployments, err := k8s.getDeployments(ctx, namespace)
-	if err != nil {
-		return nil, err
-	}
-	statefulSets, err := k8s.getStatefulSets(ctx, namespace)
-	if err != nil {
-		return nil, err
-	}
-	cronJobs, err := k8s.getCronJobs(ctx, namespace)
-	if err != nil {
+	g, ctxGroup := errgroup.WithContext(ctx)
+
+	var deployments, statefulSets, cronJobs map[string]kubesleep.Suspendable
+
+	g.Go(func() error {
+		var err error
+		deployments, err = k8s.getDeployments(ctxGroup, namespace)
+		return err
+	})
+	g.Go(func() error {
+		var err error
+		statefulSets, err = k8s.getStatefulSets(ctxGroup, namespace)
+		return err
+	})
+	g.Go(func() error {
+		var err error
+		cronJobs, err = k8s.getCronJobs(ctxGroup, namespace)
+		return err
+	})
+	if err := g.Wait(); err != nil {
 		return nil, err
 	}
 
